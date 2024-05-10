dc1dd7b7748a66cf4947e1ec3053a70eec800f55
Qnxy <qnxy1997@gmail.com>
2024-05-10 23:37:18 +0800

    增加 测试使用 TestTable 类

---
+++ mall-repository/repository-spring-jdbc/src/test/java/com/ck/mall/repository/TestTable.java
@@ -0,0 +1,16 @@
+package com.ck.mall.repository;
+
+import lombok.Data;
+
+/**
+ * 测试表
+ *
+ * @author Qnxy
+ */
+@Data
+public class TestTable {
+    private Long id;
+    private String username;
+    private Integer age;
+
+}
